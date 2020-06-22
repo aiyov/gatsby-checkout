398f8bdd6c3270d89b21df3411e7de44811d1c23
leonli <leonli@aaxischina.com>
2020-06-22 09:36:50 +0800

    first commit

---
+++ src/components/marketing/seo.tsx
@@ -0,0 +1,23 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+// @ts-ignore
+import React from "react"
+
+function SEO({ description, lang, meta, title }) {
+    return <div />
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+}
+
+
+
+export default SEO
